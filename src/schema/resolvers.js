457d0092dfb4c806c7888b955aaa8af6d096f01a
Arif Pirxada <arifpirxada@gmail.com>
2024-07-17 10:46:22 +0530

    Update - Created todo API, specific user, todos

--- src/schema/resolvers.js
+++ src/schema/resolvers.js
@@ -14,6 +14,7 @@ const getData = (filePath) => {
 
 const resolvers = {
   Query: {
+    // User Queries =>
     users: async () => {
       try {
         const userData = await getData(path.join(__dirname, '../data', 'users.json'));
@@ -23,6 +24,40 @@ const resolvers = {
         throw new Error('Error fetching user data');
       }
     },
+
+    user: async (parent, args) => {
+      try {
+        const userData = await getData(path.join(__dirname, '../data', 'users.json'));
+        const user = userData.find(user => user.id == args.id);
+        return user;
+      } catch (error) {
+        console.error(error);
+        throw new Error('Error fetching user data');
+      }
+    },
+
+
+    // Todo Queries 
+    todos: async () => {
+      try {
+        const todoData = await getData(path.join(__dirname, '../data', 'todos.json'));
+        return todoData;
+      } catch (error) {
+        console.error(error);
+        throw new Error('Error fetching todos data');
+      }
+    },
+
+    userSpecificTodos: async (parent, args) => {
+      try {
+        const todoData = await getData(path.join(__dirname, '../data', 'todos.json'));
+        const specificTodos = todoData.filter(todo => todo.userId == args.userId)
+        return specificTodos;
+      } catch (error) {
+        console.error(error);
+        throw new Error('Error fetching todos data');
+      }
+    }
   },
 };
 
--- src/schema/type-definitions.js
+++ src/schema/type-definitions.js
@@ -12,9 +12,21 @@ const typeDefs = gql`
         image: String        
     }
 
+    type Todos {
+        id: ID!
+        todo: String!
+        completed: Boolean!
+        userId: ID!
+    }
+
     type Query {
         users: [User!]!
+        user(id: ID!): User
+        todos: [Todos!]!
+        userSpecificTodos(userId: ID!): [Todos!]!
     }
 `;
+
+// These exclamation marks in Query ( [User!]! ) say that User can not be null, and the outer mark says that the array ca not be null but it can be empty.
 
 module.exports = { typeDefs };
