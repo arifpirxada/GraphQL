8f2d707ce6b274fe896bc9a1a9233eb35c9a96b5
Arif Pirxada <arifpirxada@gmail.com>
2024-07-17 16:56:47 +0530

    Update - GraphQL Mutations

--- src/schema/resolvers.js
+++ src/schema/resolvers.js
@@ -12,6 +12,19 @@ const getData = (filePath) => {
   });
 };
 
+const setData = (filePath, data) => {
+  return new Promise((resolve, reject) => {
+    const jsonData = JSON.stringify(data, null, 4);
+    fs.writeFile(filePath, jsonData, (err) => {
+      if (err) {
+        console.error('Error writing to file', err);
+        return reject(err);
+      }
+      resolve(true)
+    });
+  });
+}
+
 const resolvers = {
   Query: {
     // User Queries =>
@@ -51,7 +64,7 @@ const resolvers = {
     userSpecificTodos: async (parent, args) => {
       try {
         const todoData = await getData(path.join(__dirname, '../data', 'todos.json'));
-        const specificTodos = todoData.filter(todo => todo.userId == args.userId)
+        const specificTodos = todoData.filter(todo => todo.userId == args.userId);
         return specificTodos;
       } catch (error) {
         console.error(error);
@@ -59,6 +72,53 @@ const resolvers = {
       }
     }
   },
+  Mutation: {
+    createUser: async (parent, args) => {
+      try {
+        const userData = await getData(path.join(__dirname, '../data', 'users.json'));
+        const user = args.input;
+        user.id = userData.length + 2;
+        userData.push(user)
+
+        if (setData(path.join(__dirname, '../data', 'users.json'), userData)) {
+          console.log("User has been created!")
+        }
+
+        return "user"
+      } catch (error) {
+        console.error(error);
+        throw new Error('Error occured while creating user');
+      }
+    },
+    updateUser: async (parent, args) => {
+      try {
+        const userData = await getData(path.join(__dirname, '../data', 'users.json'));
+        userToBeUpdated = args.input
+        let returnUser;
+        const updatedUserData = userData.map(user => {
+          if (user.id == userToBeUpdated.id) {
+            returnUser = user;
+            return {
+              ...user,
+              firstName: userToBeUpdated.firstName,
+              lastName: userToBeUpdated.lastName
+            };
+          } else {
+            return user;
+          }
+        });
+
+        if (setData(path.join(__dirname, '../data', 'users.json'), updatedUserData)) {
+          console.log("User has been updated!")
+        }
+
+        return returnUser;
+      } catch (error) {
+        console.error(error);
+        throw new Error('Error occured while updating user');
+      }
+    }
+  }
 };
 
 module.exports = { resolvers };
--- src/schema/type-definitions.js
+++ src/schema/type-definitions.js
@@ -25,8 +25,29 @@ const typeDefs = gql`
         todos: [Todos!]!
         userSpecificTodos(userId: ID!): [Todos!]!
     }
+
+    input CreateUserInput {
+        firstName: String!
+        lastName: String!
+        age: Int
+        gender: String
+        email: String!
+        password: String!
+        image: String = "https://images.unsplash.com/photo-1522075469751-3a6694fb2f61?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHVzZXIlMjBwcm9maWxlfGVufDB8fDB8fHww"
+    }
+
+    input UpdateUserNameInput {
+        id: ID!
+        firstName: String!
+        lastName: String!
+    }
+
+    type Mutation {
+        createUser(input: CreateUserInput): User
+        updateUser(input: UpdateUserNameInput): User
+    }
 `;
 
-// These exclamation marks in Query ( [User!]! ) say that User can not be null, and the outer mark says that the array ca not be null but it can be empty.
+// Image in createUserInput has a default value
 
 module.exports = { typeDefs };
